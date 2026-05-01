a57640a43556bb9ccebb5024f5f3557c14689a77
OpenSoftware-World <143949134+OpenSoftware-World@users.noreply.github.com>
2026-05-01 18:00:47 +0300

    OpenKernel 3.0 has been released.

--- Kernel/openkernel_information.h
+++ Kernel/openkernel_information.h
@@ -2,8 +2,8 @@
 #define OPENKERNEL_INFORMATION_H
 
 #define KRNL_NAME "OpenKernel"
-#define KRNL_VER "2.0"
+#define KRNL_VER "3.0"
 #define KRNL_ARCH "x86 (32-bit)"
-#define KRNL_LAST_COMMIT "6e7df84"
+#define KRNL_LAST_COMMIT "16593b9"
 
 #endif
