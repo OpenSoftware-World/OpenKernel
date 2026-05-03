0bf7b98a6bdd7729da4e269b7fd41e8484a8242f
OpenSoftware-World <143949134+OpenSoftware-World@users.noreply.github.com>
2026-05-03 17:43:25 +0300

    New features have been added for the Serial (COM1_PORT) driver, and a driver protection feature has also been added.

--- Drivers/Serial/serial.c
+++ Drivers/Serial/serial.c
@@ -3,7 +3,10 @@
 #define SERIAL_LINE_STATUS_DATA_READY 0x01
 #define SERIAL_LINE_STATUS_THRE 0x20
 
+static bool_t serial_status = true;
+
 void serial_init() {
+    serial_status = true;
     outb(COM1_PORT + 1, 0x00);
     outb(COM1_PORT + 3, 0x80);
     outb(COM1_PORT + 0, 0x03);
@@ -24,11 +27,13 @@ int serial_is_transmit_empty() {
 }
 
 void serial_print_char(char c) {
+    if (!serial_status) return;
     while (!serial_is_transmit_empty());
     outb(COM1_PORT, c);
 }
 
 void serial_print_string(const char *string) {
+    if (!serial_status) return;
     while (*string) {
         if (*string == '\n') {
             serial_print_char('\r');
@@ -40,4 +45,78 @@ void serial_print_string(const char *string) {
 char serial_read_char() {
     while (!(inb(COM1_PORT + 5) & SERIAL_LINE_STATUS_DATA_READY));
     return inb(COM1_PORT);
+}
+
+int serial_received() {
+    return inb(COM1_LSR) & 1;
+}
+
+char serial_read_char() {
+    while (serial_received() == 0);
+    return inb(COM1_PORT);
+}
+
+int serial_try_read_char(char *c) {
+    if (!(inb(COM1_LSR) & 1)) {
+        return 0;
+    }
+    *c = inb(COM1_PORT);
+    return 1;
+}
+
+int serial_has_error() {
+    uint8_t stat = inb(COM1_LSR);
+    return stat & (0x0E | 0x80);
+}
+
+int serial_get_errors() {
+    return inb(COM1_LSR) & (SERIAL_ERR_OVERRUN | SERIAL_ERR_PARITY | SERIAL_ERR_FRAME | SERIAL_ERROR_FIFO);
+}
+
+void serial_clear_errors() {
+    inb(COM1_LSR);
+}
+
+void serial_log(const char* tag, const char* msg) {
+    serial_print_string("[");
+    serial_print_string(tag);
+    serial_print_string("]");
+    serial_print_string(msg);
+    serial_print_string("\n");
+}
+
+void serial_success(const char* msg) {
+    serial_log("SUCCESS", msg);
+}
+
+void serial_error(const char* msg) {
+    serial_log("ERROR", msg);
+}
+
+void serial_warn(const char* msg) {
+    serial_log("WARNING", msg);
+}
+
+void serial_info(const char* msg) {
+    serial_log("INFO", msg);
+}
+
+void serial_unknown(const char* msg) {
+    serial_log("UNKNOWN", msg);
+}
+
+void serial_set_status(bool_t val) {
+    serial_status = val;
+}
+
+void serial_loopback_test() {
+    outb(COM1_MCR, 0x1E);
+    outb(COM1_PORT, 0xAE);
+    if (inb(COM1_PORT) != 0xAE) {
+        serial_error("Loopback test FAILED.");
+    }
+    else {
+        serial_success("Loopback test OK.");
+    }
+    outb(COMR_MCR, 0x0);
 }
--- Drivers/Serial/serial.h
+++ Drivers/Serial/serial.h
@@ -5,16 +5,36 @@
 #include <OpenKernel/SystemLib/SystemIO/io.h>
 
 #define COM1_PORT 0x3F8
+#define COM1_LSR (COM1_PORT + 5)
+#define SERIAL_ERR_OVERRUN 0x02
+#define SERIAL_ERR_PARITY 0x04
+#define SERIAL_ERR_FRAME 0x08
+#define SERIAL_ERROR_FIFO 0x80
+#define COM1_MCR (COM1_PORT + 4)
 
 void serial_init();
 int serial_is_transmit_empty();
 void serial_print_char(char c);
 void serial_print_string(const char *string);
+char serial_read_char();
+int serial_received();
+int serial_try_read_char(char *c);
+int serial_has_error();
+int serial_get_error();
+void serial_clear_errors();
+void serial_log(const char* tag, const char* msg);
+void serial_success(const char* msg);
+void serial_error(const char* msg);
+void serial_warn(const char* msg);
+void serial_info(const char* msg);
+void serial_unknown(const char* msg);
+void serial_set_status(bool_t val);
+void serial_loopback_test();
 
 #define SERIAL_DRIVER_NAME "OpenKernel Serial (COM1_PORT) Driver"
-#define SERIAL_DRIVER_VER "0.1"
-#define SERIAL_DRIVER_DESC "A simple VGA text mode driver for OpenKernel"
+#define SERIAL_DRIVER_VER "0.2"
+#define SERIAL_DRIVER_DESC "A Serial (COM1_PORT) driver for OpenKernel"
 #define SERIAL_DRIVER_AUTHOR "OpenSoftware-World"
-#define SERIAL_DRIVER_KRNL_VER "2.0"
+#define SERIAL_DRIVER_KRNL_VER "3.0"
 
 #endif
