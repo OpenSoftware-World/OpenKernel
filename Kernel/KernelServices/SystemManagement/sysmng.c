bd46e285e8850a031de89e025f6f6256d4f523bf
OpenSoftware-World <143949134+OpenSoftware-World@users.noreply.github.com>
2026-05-23 18:14:21 +0300

    The KernelFunc and system shutdown functions have been made more secure.

--- Kernel/KernelServices/SystemManagement/sysmng.c
+++ Kernel/KernelServices/SystemManagement/sysmng.c
@@ -1,17 +1,18 @@
 #include "sysmng.h"
+#include <OpenKernel/SystemLib/Std/std.h>
 
 void sys_next_status(const char *mode, uint32_t seconds) {
     /*
     S: Shutdown
     R: Reboot
     */
-   sleep(seconds);
-    if (mode[0] == 'S') {
+    sleep(seconds);
+    if (mode[0] == 'S' && strcmp(mode, "shutdown") == 0) {
         outb(0x604, 0x00);
         sys_wait();
         outb(0x605, 0x20);
     }
-    else if(mode[0] == 'R') {
+    else if(mode[0] == 'R' && strcmp(mode, "reboot") == 0) {
         cli();
         while (inb(0x64) & 0x02);
         outb(0x64, 0xFE);
--- SystemLib/KernelFunc/kernel_func.c
+++ SystemLib/KernelFunc/kernel_func.c
@@ -1,4 +1,5 @@
 #include "kernel_func.h"
+#include <OpenKernel/SystemLib/Memory/mem.h>
 
 #define MAX_FUNC 256
 
@@ -9,8 +10,11 @@ typedef struct {
 
 static kfunc_entry_t ftable[MAX_FUNC];
 static int kfunc_count = 0;
+static int kfunc_s = 0;
+static bool_t kernel_func_enabled = false;
 
 void register_func(const char *name, kfunc_t func) {
+    if (kernel_func_enabled == false) return;
     if (kfunc_count >= MAX_FUNC) {
         return;
     }
@@ -20,6 +24,7 @@ void register_func(const char *name, kfunc_t func) {
 }
 
 void exec_func(const char *func_name) {
+    if (kernel_func_enabled == false) return;
     for (int i = 0; i < kfunc_count; i++) {
         if (strcmp(ftable[i].name, func_name) == 0) {
             ftable[i].func();
@@ -29,5 +34,21 @@ void exec_func(const char *func_name) {
 }
 
 void exec_command(const char *command_name) {
+    if (kernel_func_enabled == false) return;
     exec_func(command_name);
+}
+
+void kernel_func_init() {
+    memset(ftable, 0, sizeof(ftable));
+    kfunc_count = 0;
+}
+
+void KernelFunc_O() {
+    if (kernel_func_enabled == true) return;
+    kernel_func_enabled = true;
+}
+
+void KernelFunc_C() {
+    if (kernel_func_enabled == false) return;
+    kernel_func_enabled = false;
 }
